e6ed22617eed579712c8c2e92593500daf6ac502
FrancoMuVa <muvafranco@gmail.com>
2024-01-06 05:09:53 +0000

    fix: fixed argument name "max_delay"

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,21 +3,18 @@
     async routine called wait_n that takes in 2 int arguments n and max_delay.
     spawn wait_random n times with the specified max_delay.
 """
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_deley: int) -> List[float]:
     """ Returns the list of all the delays in ascending order """
-    deleys: List[float] = []
+    deleys: List = [wait_random(max_deley) for _ in range(n)]
+    all_deleys: List = await asyncio.gather(*deleys)
+    sorted_deleys: List = []
     for _ in range(n):
-        num = await wait_random(max_deley)
-        deleys.append(num)
-
-    sorted_deleys: List[float] = []
-    for _ in range(n):
-        min_deley = min(deleys)
+        min_deley = min(all_deleys)
         sorted_deleys.append(min_deley)
-        deleys.remove(min_deley)
-
+        all_deleys.remove(min_deley)
     return sorted_deleys
