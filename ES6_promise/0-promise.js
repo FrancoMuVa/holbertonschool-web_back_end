c6ff3c7410f335919bd013dda126cf66e1dad924
FrancoMuVa <muvafranco@gmail.com>
2024-01-25 22:15:12 +0000

    fix: eslint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('ok');
-        }, 300);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('ok');
+    }, 300);
+  });
 }
