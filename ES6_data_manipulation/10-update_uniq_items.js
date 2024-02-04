35fa1e4cb172d58f2172c7344f6ce80eb75f97e6
FrancoMuVa <muvafranco@gmail.com>
2024-02-04 18:18:27 +0000

    fix: task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   for (const item of map) {
     if (item[1] === 1) {
       item[1] = 100;
